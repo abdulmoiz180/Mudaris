271f063a51fda5f1abd4dd05f04c6f2372685699
hassanmal69 <158709739+hassanmal69@users.noreply.github.com>
2024-09-03 22:02:13 +0500

    Add files via upload

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./home.css"
+import HeroPage from '../../components/Hero_Page/Index'
 import E_Learn from '../../components/E_Learn'
 import Footer from '../../components/Footer'
 const Home = () => {
   return (
     <section>
+      <HeroPage/>
       <E_Learn />
       <Footer />
     </section>
