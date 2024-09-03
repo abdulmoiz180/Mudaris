f7c3b4bc46219e0260018f95ad3202796c86e7af
hassanmal69 <hassantahir688@gmail.com>
2024-09-04 01:58:40 +0600

    hero component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import "./home.css"
 import E_Learn from '../../components/E_Learn'
 import Footer from '../../components/Footer'
+import Hero from '../../components/Hero/Index'
 const Home = () => {
   return (
     <section>
+      <Hero/>
       <E_Learn />
       <Footer />
     </section>
