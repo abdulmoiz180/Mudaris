08ce2d2246ad8198d51f0423394568b39fc15f40
abdulmoiz180 <moiza8684@gmail.com>
2024-09-04 14:32:00 +0500

    Faq complete...

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import "./home.css"
 import E_Learn from '../../components/E_Learn'
 import Footer from '../../components/Footer'
+import FAQ from '../../components/Accordion'
 const Home = () => {
   return (
-    <section>
-      <E_Learn />
-      <Footer />
+    <section className='home-page primary-bg '>
+     {/* <E_Learn /> */}
+      {/*  <Footer /> */}
+      <FAQ />
     </section>
   )
 }
