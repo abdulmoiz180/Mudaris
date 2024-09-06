b87e72e967df9661ef7128c0633897b8cfe8064f
Talha408 <fatandhuge888@gmail.com>
2024-09-07 03:39:55 +0700

    Made the GetStarted section with responsiveness

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,17 +3,21 @@ import E_Learn from "../../components/E_Learn";
 import Footer from "../../components/Footer";
 import Hero from "../../components/Hero/Index";
 import FAQ from "../../components/Accordion";
+import GetStart from "../../components/GetStarted";
 import PriceCards from "../../components/PriceCards/Index";
 import Courses from "../../components/Courses";
+// import { GetStarted } from "../../Seed";
 
 const Home = () => {
   return (
     <section className="home-page column primary-bg">
       <Hero/> 
+     
       <Courses />
        <E_Learn />
       <PriceCards/>
       <FAQ />
+      <GetStart/>
       <Footer />
     </section>
   );
