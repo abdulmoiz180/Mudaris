419ae66770a26539076e9115e218f54bedbb0360
abdulmoiz180 <moiza8684@gmail.com>
2024-09-07 18:56:35 +0500

    main has some issues to fix...

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,28 +1,21 @@
-
 import E_Learn from "../../components/E_Learn";
-import Footer from "../../components/Footer";
 import Hero from "../../components/Hero/Index";
 import FAQ from "../../components/Accordion";
 import GetStart from "../../components/GetStarted";
 import PriceCards from "../../components/PriceCards/Index";
 import Courses from "../../components/Courses";
-import { GetStarted } from "../../Seed";
-
 import "./home.css";
 import Review from "../../components/Review/Index";
 const Home = () => {
   return (
     <section className="home-page column primary-bg">
-       <ResponsiveAppBar/>
-    
       <Hero />
       <Courses />
       <Review />
       <E_Learn />
       <PriceCards />
       <FAQ />
-      <GetStart/>
-      <Footer />
+      <GetStart />
     </section>
   );
 };
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import Home from "../Pages/Home";
+import ResponsiveAppBar from "../components/NavBar";
+import Footer from "../components/Footer";
 const Router = () => {
   return (
     <BrowserRouter>
+      <ResponsiveAppBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
