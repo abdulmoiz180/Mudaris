af3ac806cf82deb1a4984e515dad4b21b614af1c
Talha408 <fatandhuge888@gmail.com>
2024-09-07 16:52:14 +0700

    Testimonials without responsiveness

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -6,10 +6,12 @@ import PriceCards from "../../components/PriceCards/Index";
 import Courses from "../../components/Courses";
 import "./home.css";
 import Review from "../../components/Review/Index";
+import Testimonials from "../../components/Testimonials/Testimonials";
 const Home = () => {
   return (
     <section className="home-page column primary-bg">
       <Hero />
+      <Testimonials/>
       <Courses />
       <Review />
       <E_Learn />
