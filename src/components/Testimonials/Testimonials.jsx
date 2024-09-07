ec218f93a0f2fa45c7cd926ebf8c673c05152f0f
abdulmoiz180 <moiza8684@gmail.com>
2024-09-07 17:00:07 +0500

    little responsiveness

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import "./Testimonials.css";
-import "../../global.css"
-import Client from "../../assets/Images/Client.png"
-import Ellipse7 from "../../assets/Images/Ellipse7.png"
+import "../../global.css";
+import Client from "../../assets/Images/Client.png";
+import Ellipse7 from "../../assets/Images/Ellipse7.png";
 import Avatar from "../../assets/Images/Avatar.png";
 
 function Testimonials() {
   return (
-    <Box className="container primary-bg">
+    <Box className="container">
       <Box className="box1">
         <h1 className="clients clr-white inter">Our Clients</h1>
         <p className="clients clr-white inter">
@@ -16,48 +16,24 @@ function Testimonials() {
           solutions.
         </p>
       </Box>
-      <Box className = "box2">
+      <Box className="box2">
         <div className="image-container">
-        <img className = "ellipse" src={Ellipse7} alt="img"/>
-          <img className = "square-image" src={Client} alt="img"/>
-          <img src={Avatar} className = "avatar" alt="img"/>
-          
+          <img className="ellipse" src={Ellipse7} alt="img" />
+          <img className="square-image" src={Client} alt="img" />
+          <img src={Avatar} className="avatar" alt="img" />
         </div>
         <div className="client-review">
-          <p className="clr-white inter clienttext">This platform has revolutionized our training programs. The results speak for themselves!</p>
+          <p className="clr-white inter clienttext">
+            This platform has revolutionized our training programs. The results
+            speak for themselves!
+          </p>
           <p className="clr-white inter clientname">Talia Taylor</p>
-          <p className="lightgray inter clientrole">John Doe, CEO of @ Quantum</p>
+          <p className="lightgray inter clientrole">
+            John Doe, CEO of @ Quantum
+          </p>
         </div>
       </Box>
     </Box>
-
-  //   <Container className="container">
-  //   {clientdata.map((client, index) => (
-  //     <Box key={index}> {/* Use Box instead of React.Fragment */}
-  //       <Box className="box1">
-  //         <h1 className="clients primaryFont">{client.title}</h1>
-  //         <p className="clients secondaryFont">{client.description}</p>
-  //       </Box>
-  //       <Box className="box2">
-  //         <div className="image-container">
-  //           <img className="square-image" src={client.img} alt="Client"/>
-  //         </div>
-  //         <div className="client-review">
-  //           <p className="primary-font-clr secondaryFont review">
-  //             {client.platformdescription}
-  //           </p>
-  //           <p className="primary-font-clr secondaryFont clientname">
-  //             {client.clientname}
-  //           </p>
-  //           <p className="secondary-font-clr secondaryFont clientrole">
-  //             {client.clientrole}
-  //           </p>
-  //         </div>
-  //       </Box>
-  //     </Box>
-  //   ))}
-  // </Container>
-
   );
 }
 
