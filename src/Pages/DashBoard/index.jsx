796fb3a3c9315f8cf10490d4a8479acb74f9c6f4
Talha408 <fatandhuge888@gmail.com>
2024-09-28 23:56:39 +0700

    Component is a bit ok but wihout right colors for now

--- src/Pages/DashBoard/index.jsx
+++ src/Pages/DashBoard/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import DashboardLayoutBasic from './components/SideBar'
-
+import AboutCourses from './components/courses/AboutCourses'
 export const Dashboard = () => {
   return (
     <div className='clr-white'>
-    <DashboardLayoutBasic/></div>
+    <DashboardLayoutBasic/>
+    <AboutCourses/>
+    </div>
+   
   )
 }
