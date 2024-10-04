b9cefe2393f2777109314d4c72517a1202acbe78
abdulmoiz180 <moiza8684@gmail.com>
2024-10-05 03:44:53 +0500

    added dot env fiel

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDJTJyq-mBNakQofCmcM5ChvzSPxsZfPqc",
-  authDomain: "elearn-8ae65.firebaseapp.com",
-  projectId: "elearn-8ae65",
-  storageBucket: "elearn-8ae65.appspot.com",
-  messagingSenderId: "976248256990",
-  appId: "1:976248256990:web:a5352e5649764a67cb454f",
-  measurementId: "G-W9CPSY022P",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
