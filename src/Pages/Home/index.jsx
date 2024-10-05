25e1268a4ae5cb692af4c2b7b1bde9f717858221
abdulmoiz180 <moiza8684@gmail.com>
2024-10-06 00:17:34 +0500

    new build

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,39 +4,27 @@ import Hero from "./components/Hero/Index";
 import FAQ from "./components/Accordion";
 import GetStart from "./components/GetStarted";
 import PriceCards from "./components/PriceCards/Index";
-import Courses from "./components/Courses";
+import LineCards from "./components/ReadIt";
 import Review from "./components/Review/Index";
-import Testimonials from "./components/Testimonials/Testimonials";
-import { useLanguage } from "../../globalContext/GlobalProvider";
 import GettoKnow from "./components/GettoKnow/Index";
 import DigitalEducation from "./components/DigitalEducationSlider";
 import Community from "./components/Community";
 import WhatYouGet from "./components/WhatYouGet";
 import GetaJob from "./components/GetaJob";
-import Participate from "./components/Participate";
-import Classes from "./components/Classes";
-import { DataScience } from "./components/DataScienceCourse";
 import "./home.css";
 const Home = () => {
-  const { language } = useLanguage(); // Moved inside the function
-
   return (
-    <section
-      className="home-page column primary-bg"
-      dir={language === "english" ? "ltr" : "rtl"} // Set text direction based on language
-    >
+    <section className="home-page column primary-bg">
       <Hero />
-      <Testimonials />
-      <Review />
-      <E_Learn />
-      <PriceCards />
-      <Participate />
-      <Classes />
+      <GetaJob />
       <DigitalEducation />
       <Community />
+      <LineCards />
       <WhatYouGet />
+      <PriceCards />
       <GettoKnow />
-      <DataScience />
+      <Review />
+      <E_Learn />
       <FAQ />
       <GetStart />
     </section>
