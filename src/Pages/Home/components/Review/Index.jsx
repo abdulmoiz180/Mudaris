6261ed1e3e140e8d20f3ebcbaf1b6b04e7e938cd
abdulmoiz180 <moiza8684@gmail.com>
2024-10-05 08:35:32 +0500

    attepmt 14

--- src/Pages/Home/components/Review/Index.jsx
+++ src/Pages/Home/components/Review/Index.jsx
@@ -3,7 +3,7 @@ import { Box, Typography, Button } from "@mui/material";
 import { useLanguage } from "../../../../globalContext/GlobalProvider";
 import "./Review.css";
 import Base from "../../../../assets/Images/Base.png";
-
+import Brightstar from "@assets/Icons/brightstarfilled.png";
 const Review = () => {
   const [more, setMore] = useState(true);
   const wrap = useRef(null);
@@ -62,7 +62,7 @@ const Review = () => {
                     </Box>
                     <Box component="div" className="ratingsImageStar">
                       {review.brightstar && (
-                        <img src={review.brightstar} alt="Rating Star" />
+                        <img src={Brightstar} alt="Rating Star" />
                       )}
                     </Box>
                   </Box>
