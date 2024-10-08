9fba98bbdf3509909f31615c648b9ccb4a037c83
abdulmoiz180 <moiza8684@gmail.com>
2024-10-09 04:16:33 +0500

    uaa

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/Mudaris/",
+  base: "/Mudaris_academy/",
   plugins: [react()],
   resolve: {
     alias: {
